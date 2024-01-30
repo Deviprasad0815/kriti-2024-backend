5a90a5410aef5988a32637a79a07add316492622
botketan <ketansingh918@gmail.com>
2024-01-30 19:37:01 +0530

    Login Bug

--- controller/jwt.js
+++ controller/jwt.js
@@ -6,7 +6,7 @@ const login = async (req,res)=>{
     try{
         const check = await hostels.findOne({username:req.body.username})
         
-        if(check.password === req.body.password){
+        if(check && check.password === req.body.password){
             jwt.sign({check},'kriti2024',{
                 expiresIn: 60*60*24,
             },(err,token)=>{
@@ -23,7 +23,7 @@ const login = async (req,res)=>{
             })
         }
         else{
-            res.sendStatus(401)
+            return res.sendStatus(401)
         }
     }
     catch(e){
