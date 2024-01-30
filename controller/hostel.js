2f315e122e330b5c71d617096d38bfffe5ca5988
botketan <ketansingh918@gmail.com>
2024-01-30 17:32:38 +0530

    Minor changes

--- controller/hostel.js
+++ controller/hostel.js
@@ -1,6 +1,7 @@
 
 const PS = require('../models/PSschema');
 const hostels = require('../models/hostel');
+const mongoose = require('mongoose');
 const psRegister = async (req, res) => {
     try
     {
@@ -26,15 +27,20 @@ const psRegister = async (req, res) => {
 
 const findPS = async (req,res)=>{
     try{
-        const ps = await PS.findOne({psName:req.body.psName,hostel:req.userId});
-        if(!ps){
-            res.sendStatus(201)
-        }
-        else{
-            res.status(200).send({ps})
-        }
+        console.log(req.body);
+        const ps = await PS.find({psName:req.body.psName});
+        console.log(req.userId);
+        ps.forEach((p)=>{
+            console.log(p);
+            if(p.hostel.equals(req.userId)){
+                res.status(200).send({ps})
+                return;
+            }
+        })
+            res.sendStatus(201);
     }
     catch(err){
+        console.log(err);
         res.send(500,err)
     }
 }
