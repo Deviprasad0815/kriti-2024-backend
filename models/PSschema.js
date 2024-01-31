c0a6807664d822aa9bfbc935a3922b78a23b575b
DeveshKamble <d.kamble@iitg.ac.in>
2024-01-31 20:08:19 +0530

    Changed PS Schema

--- models/PSschema.js
+++ models/PSschema.js
@@ -1,44 +1,53 @@
 const mongoose = require("mongoose");
 
-const psSchema=new mongoose.Schema({
-    psName:{
-        type:String,
-        required:true
+const psSchema = new mongoose.Schema({
+  psName: {
+    type: String,
+    required: true,
+  },
+
+  hostel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hostels",
+  },
+  hostelName: {
+    type: String,
+    required: true,
+  },
+  studentsData: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rollNo: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      phoneNumber: {
+        type: String,
+        required: true,
+      },
+      discordID: {
+        type: String,
+        required: true,
+      },
+      programme: {
+        type: String,
+        required: true,
+      },
+      year: {
+        type: String,
+        required: true,
+      },
     },
-    
-    hostel:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Hostels",
-    },
-    hostelName:{
-        type:String,
-        required:true
-    },
-    studentsData:[{
-        
-        name:{
-            type:String,
-            required:true
-        },
-        rollNo:{
-            type:String,
-            required:true
-        },
-        email:{
-            type:String,
-            required:true
-        },
-        year:{
-            type:String,
-            required:true
-        }
+  ],
+});
 
+const PS = mongoose.model("PS", psSchema);
 
-    }]
-
-
-})
-
-const PS = mongoose.model("PS",psSchema)
-
-module.exports = PS
+module.exports = PS;
